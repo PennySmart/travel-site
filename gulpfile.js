1c834c2d9e3ce07e2430115f93e374a593c2710a
Paula <paula.clarke@hotmail.com>
2018-05-14 09:30:52 -0400

    source code added

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,9 @@
-var gulp = require('gulp'),
- watch = require('gulp-watch');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+  
+ 
+ 
 
-gulp.task('default', function(){
-    console.log("Hurray you created a Gulp task");
-});
 
-gulp.task('html', function(){
-    console.log("Imagine something useful being done to your HTML here.");
-});
-gulp.task('watch', function() {
-    watch('./app/index.html', function(){
-        gulp.start('html');
-    })
-});
+
+
